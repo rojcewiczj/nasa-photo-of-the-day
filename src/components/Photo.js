1ec863ff2fa0ba01b0685e273bb93298aae4d6a4
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-14 15:07:52 -0500

    trying to create a function to give a random date

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -5,10 +5,16 @@ import PhotoCard from "./PhotoCard";
 
 const Photo = ({limit}) => {
     const [photo, setPhoto] = useState([]);
-    
+    const [date, setdate] = useState();
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    const randomDate = `${Math.floor((Math.random() * 10) + 1)}`
     useEffect(() => {
         axios
-          .get(`https://api.nasa.gov/planetary/apod?api_key=KMacZdIarL5RiK4YQbg2ypnayh0fDPPCvqVU7uLq`)
+          .get(`https://api.nasa.gov/planetary/apod?api_key=KMacZdIarL5RiK4YQbg2ypnayh0fDPPCvqVU7uLq${date}`)
          
           .then(response => {
                console.log(response);
@@ -16,10 +22,10 @@ const Photo = ({limit}) => {
             console.log(NasaPhoto);
             setPhoto(NasaPhoto);
           })
-      }, [])
+      }, [date])
       return (
           <div className="photo-container">
-          
+           {/* <button onClick={() => setDate("date")}>{viewingDate}</button>*/}
                <PhotoCard data = {photo} />
          
 
