e690cc34ba5718b3e283003a4afc74d54b379a0b
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-14 17:46:52 -0500

    built button to generate random pages

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -3,18 +3,23 @@ import axios from "axios";
 import PhotoCard from "./PhotoCard";
 
 
-const Photo = ({limit}) => {
-    const [photo, setPhoto] = useState([]);
-    const [date, setdate] = useState();
-    function getRandomInt(min, max) {
+
+function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    const randomDate = `${Math.floor((Math.random() * 10) + 1)}`
+ const randomDate = () => `apod?api_key=KMacZdIarL5RiK4YQbg2ypnayh0fDPPCvqVU7uLq&date=201${getRandomInt(2,8)}-0${getRandomInt(1,9)}-${getRandomInt(0,2)}${getRandomInt(1,8)}`;
+
+const Photo = ({limit}) => {
+    const [photo, setPhoto] = useState([]);
+    const [date, setDate] = useState('apod?api_key=KMacZdIarL5RiK4YQbg2ypnayh0fDPPCvqVU7uLq&date=2015-05-24');
+    const [Name, setClassName] = useState('dropdown');
+   const [newName, setNewClassName] = useState()
+    console.log(randomDate);
     useEffect(() => {
         axios
-          .get(`https://api.nasa.gov/planetary/apod?api_key=KMacZdIarL5RiK4YQbg2ypnayh0fDPPCvqVU7uLq${date}`)
+          .get(`https://api.nasa.gov/planetary/${date}`)
          
           .then(response => {
                console.log(response);
@@ -23,9 +28,19 @@ const Photo = ({limit}) => {
             setPhoto(NasaPhoto);
           })
       }, [date])
+
+    
+
+
+
       return (
           <div className="photo-container">
-           {/* <button onClick={() => setDate("date")}>{viewingDate}</button>*/}
+
+           
+               <button className= {Name}   onMouseOver={() => setClassName("dropdown-menu")} onMouseLeave= {()=> setClassName("dropdown")} 
+               onClick={() => setDate(randomDate())} >New Day!</button>
+            
+          
                <PhotoCard data = {photo} />
          
 
