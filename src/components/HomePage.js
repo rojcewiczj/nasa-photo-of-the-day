e06dfeb4dde5f30de0614e7ab0dc940ecc7df7b6
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-15 15:46:00 -0500

    working through layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -17,22 +17,51 @@ const FixedMenuLayout = () => (
     <Menu fixed='top' inverted>
       <Container>
         <Menu.Item as='a' header>
-       NASA photo-of-the-day!
+       NASA photo-of-the-day
         </Menu.Item>
         <Menu.Item as='a'>Home</Menu.Item>
 
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
         
+          <Dropdown.Menu>
+            <Dropdown.Item></Dropdown.Item>
+          </Dropdown.Menu>
+       
       </Container>
     </Menu>
 
     <Container text style={{ marginTop: '7em' }}>
-    
+    <Photo />
 
     </Container>
 
+    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+      <Container textAlign='center'>
+            
+        
+          <Grid.Column width={7}>
+            <Header inverted as='h4' content='Support NASA' />
+            <p>
+              learn how you cna help support NASA!
+            </p>
+          </Grid.Column>
+       
+        <Divider inverted section />
+        <List horizontal inverted divided link size='small'>
+          <List.Item as='a' href='#'>
+            Site Map
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Contact Us
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Terms and Conditions
+          </List.Item>
+          <List.Item as='a' href='#'>
+            Privacy Policy
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
   
   </div>
 )
